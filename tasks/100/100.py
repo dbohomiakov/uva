825fef428c38b914cb1b3df4c4285db60d0ba8ef
Dmytro Bohomiakov <dmitrij.bogomyakov@gmail.com>
2018-06-14 16:16:14 +0300

    refactor task 100

--- tasks/100/100.py
+++ tasks/100/100.py
@@ -24,10 +24,10 @@ def calculate_cycle_length(number):
     return 1 + calculate_cycle_length(next_number)
 
 
-def calculate_max_сycle_length(start_iter, finish_iter):
+def calculate_max_сycle_length(start_range, finish_range):
     max_сycle_length = 0
 
-    for i in range(start_iter, finish_iter + 1):
+    for i in range(start_range, finish_range + 1):
         max_сycle_length = max(calculate_cycle_length(i), max_сycle_length)
 
     return max_сycle_length
@@ -35,9 +35,8 @@ def calculate_max_сycle_length(start_iter, finish_iter):
 
 def main(raw_sample):
     parsed_input = parse_input(raw_sample)
-    start_iter, finish_iter = min(parsed_input), max(parsed_input)
-    max_iter_num = calculate_max_сycle_length(start_iter, finish_iter)
-    print(f'{parsed_input[0]} {parsed_input[1]} {max_iter_num}')
+    max_cycle_length = calculate_max_сycle_length(min(parsed_input), max(parsed_input))
+    print(f'{parsed_input[0]} {parsed_input[1]} {max_cycle_length}')
 
 
 if __name__ == '__main__':
