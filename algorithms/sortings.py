1f8b4660b569633c25b7125c54f2a302866b670d
Dmytro Bohomiakov <dmitrij.bogomyakov@gmail.com>
2018-06-25 20:12:00 +0300

    add merge sort

--- algorithms/sortings.py
+++ algorithms/sortings.py
@@ -34,12 +34,63 @@ def insertion_sort(arr):
 
      return arr
 
- 
+
+class MergeSort:
+    def __call__(self, arr):
+        self.sort(arr, 0, len(arr) - 1)
+        return arr
+
+    @staticmethod
+    def merge(arr, start, middle, end):
+        arr1 = arr[start: middle + 1]
+        arr2 = arr[middle: end]
+        arr1_remain = middle - start + 1
+        arr2_remain = end - middle
+        
+        for i in range(arr1_remain + arr2_remain):
+            if arr1_remain and arr2_remain:
+                if arr1[0] <= arr2[0]:
+                    arr[start + i] = arr1.pop(0)
+                    arr1_remain -= 1
+                else:
+                    arr[start + i] = arr2.pop(0)
+                    arr2_remain -= 1
+            elif arr1_remain:
+                arr[start + i] = arr1.pop(0)
+                arr1_remain -= 1
+            elif arr2_remain:
+                arr[start + i] = arr2.pop(0)
+                arr2_remain -= 1
+
+    @classmethod
+    def sort(cls, arr, start, end):
+        if not end - start:
+            return
+
+        if end - start == 1:
+            start_elem = arr[start]
+            end_elem = arr[end]
+                
+            if start_elem > end_elem:
+                arr[start], arr[end] = arr[start], arr[end]
+
+            return
+
+        middle = start + (end - start) // 2
+        
+        cls.sort(arr, start, middle)
+        cls.sort(arr, middle + 1, end)
+
+        cls.merge(arr, start, middle, end)
+    
+
+merge_sort = MergeSort()
+
+
 if __name__ == '__main__':
     # Test array
-    arr = [random.randrange(1, 100000, 1) for _ in range(10000)]
-
-    # print('Test array:', arr, sep = '\n')
+    arr = [random.randrange(1, 100000, 1) for _ in range(100000)]
+    
 
     start = time()
     selection_sort(arr[:])
@@ -48,3 +99,7 @@ if __name__ == '__main__':
     start = time()
     insertion_sort(arr[:])
     print('Sorted array insertion:', 'Time: {}'.format(time() - start), sep = '\n')
+
+    start = time()
+    merge_sort(arr[:])
+    print('Sorted array merge_sort:', 'Time: {}'.format(time() - start), sep = '\n')
